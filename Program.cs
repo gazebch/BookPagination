4c28e13031741884fb49d77f93448d00b006ca42
Gazeba <sipion46@mail.ru>
2022-08-13 11:11:49 +0300

    Разбил проект большее количество методов. Добавил tryParse на ввод количества элементов страницы

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ class Program
     {
         var mainBook = Enumerable.Range(1, 100);
         Console.WriteLine("Введите кол-во записей на одной странице");
-        var pageWidth = int.Parse(Console.ReadLine());
+        var pageLength = BookPageLength();
         while (true)
-        {
+        {            
             Catalog();
             var checkPoint = Console.ReadLine();
             if (checkPoint == "q") break;
@@ -15,7 +15,7 @@ class Program
             {
                 case "1":
                     {
-                        ViewPage(pageWidth, mainBook);
+                        ViewPage(pageLength, mainBook);
                         break;
                     }
                 default:
@@ -27,30 +27,67 @@ class Program
         }
     }
 
-    public static void ViewPage(int pageWidth, IEnumerable<int> book)  //
+    public static int BookPageLength()
+    {
+        int pageLength;
+        while (true)
+        {
+            var tryPageLength = int.TryParse(Console.ReadLine(), out pageLength);
+            if (!tryPageLength)
+            {
+                Console.WriteLine("неверно задано количество записей на странице");
+                continue;
+            }
+            else
+            {
+                return pageLength;
+            }
+        }        
+    }
+
+    public static void ViewPage(int pageWidth, IEnumerable<int> book)  // просмотр заданной страницы
     {
         var pageCount = book.Count() / pageWidth;
-        if (book.Count() % pageWidth != 0) pageCount += 1;
+        if (book.Count() % pageWidth != 0)
+        {
+            pageCount += 1;
+        }
         Console.WriteLine($"Введите номер страницы (всего страниц {pageCount})");
+        var numPage = NumberPageAccuracy(pageCount);
+        PageStructure(numPage, pageWidth, book);
+    }
+
+    public static void ViewCompositionBook(IEnumerable<int> books) //вывод страницы в консоль
+    {
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+
+    public static int NumberPageAccuracy(int pageCount) //проверка существующей страницы
+    {
         int numPage;
         while (true)
         {
             var tryNumPage = int.TryParse(Console.ReadLine(), out numPage);
-            if (tryNumPage && pageCount >= numPage && numPage > 0) break;
-            else Console.WriteLine("Такой страницы не существует. Введите верный номер страницы");
+            if (tryNumPage && pageCount >= numPage && numPage > 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Такой страницы не существует. Введите верный номер страницы");
+            }
         }
+        return numPage;
+    }
+
+    public static void PageStructure(int numPage, int pageWidth, IEnumerable<int> book) //построение страницы
+    {
         Console.WriteLine();
         var finalViewPage = book.Skip((numPage - 1) * pageWidth).Take(pageWidth);
         ViewCompositionBook(finalViewPage);
-
-    }
-
-    public static void ViewCompositionBook(IEnumerable<int> book)
-    {
-        foreach (var bookItem in book)
-        {
-            Console.WriteLine(bookItem);
-        }
     }
 
     public static void Catalog()
